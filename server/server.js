a84a4c6143da53ffe0d1f54d6cf14d8e412acd74
George  Siton <georgesiton@Georges-MacBook-Air.local>
2017-07-16 22:55:35 +0800

    Added connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../client');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+io.on('connection', (socket) => {
+  console.log('new socket');
+  socket.on('disconnect', () => {
+    console.log('Disconnected from server');
+  });
+});
+
+server.listen(3000, () => console.log("Listening on port 3000"));
