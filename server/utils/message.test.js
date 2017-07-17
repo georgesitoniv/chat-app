01807c9cb683d969aaa98de8c118bf55f7e12843
George Siton IV <georgesitoniv@Georges-MacBook-Air.local>
2017-07-17 12:47:26 +0800

    create generate message utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,8 @@
+const expect = require('expect');
+const { generateMessage } = require('./message');
+
+describe('generateMessage', () => {
+  it('should generate the correct message object', () => {
+    
+  });
+});
